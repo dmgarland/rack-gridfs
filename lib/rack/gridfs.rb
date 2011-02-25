30f205a81943df2368ca2b98234d789f3e565066
Will Ronco <william.ronco@gmail.com>
2011-02-25 11:45:49 -0700

    Now, start trying to update rack-gridfs so that it actually works with mongo > 0.19.1, instead of just claiming to

--- lib/rack/gridfs.rb
+++ lib/rack/gridfs.rb
@@ -33,9 +33,9 @@ module Rack
     end
 
     def gridfs_request(id)
-      file = Mongo::Grid.new(db).get(Mongo::ObjectID.from_string(id))
+      file = Mongo::Grid.new(db).get(BSON::ObjectID.from_string(id))
       [200, {'Content-Type' => file.content_type}, [file.read]]
-    rescue Mongo::GridError, Mongo::InvalidObjectID
+    rescue Mongo::GridError, BSON::InvalidObjectID
       [404, {'Content-Type' => 'text/plain'}, ['File not found.']]
     end
 
